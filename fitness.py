f60b322be98f7c35626756c5b6c1f654858a4ac3
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2023-03-16 19:02:27 +0100

    Fix fitness sorting bug

--- fitness.py
+++ fitness.py
@@ -9,15 +9,12 @@ import geopy.distance
 def distance_between_two_cities(city1, city2):
     c_1 = (city1[1], city1[2])
     c_2 = (city2[1], city2[2])
-    return geopy.distance.geodesic(c_1,c_2).km
+    return geopy.distance.geodesic(c_1, c_2).km
 
 
 def fitness(solution):
     score = 0
     for i in range(len(solution) - 2):
-        score += distance_between_two_cities(
-            solution[i], solution[i + 1])
-    score += distance_between_two_cities(
-        solution[-1], solution[0])
-    # negate fitness => higher fitness (less distance) is better
-    return score * -1
+        score += distance_between_two_cities(solution[i], solution[i + 1])
+    score += distance_between_two_cities(solution[-1], solution[0])
+    return score
--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import geopy.distance
 
 from fitness import *
 from random_solution_generator import *
-# from genetic_representation import *
 from mutation import *
-# from selection import *
 from crossover import *
-import numpy as np
+
 CITIES_FILE = './european_cities.csv'
 NUMBER_OF_SOLUTIONS = 80
-NUMBER_OF_SELECTED_SOLUTIONS = 10
+NUMBER_OF_SELECTED_SOLUTIONS = 5
 NUMBER_OF_GENERATIONS = 20
 
 
@@ -27,7 +25,6 @@ def load_cities():
 
 cities = load_cities()
 solutions = generate_solutions(cities, NUMBER_OF_SOLUTIONS)
-
 ''' prototype
 N = number of solutions
 n = number of top solutions to keep
@@ -48,25 +45,20 @@ repeat for X generations
 
 for i in range(NUMBER_OF_GENERATIONS):
     # sort by fitness
-    print("Generation ",i)
+    print("Generation ", i)
     solutions = sorted(solutions, key=lambda x: fitness(x))
     surviving_solutions = solutions[:NUMBER_OF_SELECTED_SOLUTIONS]
     print('Current Best: {}'.format(fitness(surviving_solutions[0])))
-    while(len(surviving_solutions) < 100):
-        i_1 = random.randint(0, NUMBER_OF_SELECTED_SOLUTIONS*2)
+    while (len(surviving_solutions) < 100):
+        i_1 = random.randint(0, NUMBER_OF_SELECTED_SOLUTIONS * 2)
         while True:
-            i_2 = random.randint(0, NUMBER_OF_SELECTED_SOLUTIONS*2)
+            i_2 = random.randint(0, NUMBER_OF_SELECTED_SOLUTIONS * 2)
             if i_2 != i_1:
                 break
 
-        cross_solutions = crossover_of_two(solutions[i_1],solutions[i_2])
+        cross_solutions = crossover_of_two(solutions[i_1], solutions[i_2])
         surviving_solutions.append(cross_solutions[0])
         surviving_solutions.append(cross_solutions[1])
-    #selected_index = np.random.exponential(NUMBER_OF_SELECTED_SOLUTIONS, size=2)
-    #crossover_solutions = crossover_of_two(surviving_solutions[selected_index[0]], surviving_solutions[selected_index[1]])
-    # add crossovered solutions
-    # crossovered_solutions = crossover(selected_solutions)
-    # surviving_solutions.extend(crossover(surviving_solutions))
     mutated_solutions = mutation(surviving_solutions)
     solutions = mutated_solutions
 print('Best solution: {}'.format(solutions[0]))

33f9b8183173ab367fb8d8f96a47fef2ad5dec28
Nico Ohler <nico.ohler@student.tugraz.at>
2023-03-16 16:16:45 +0100

    Add prototype of first iteration

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 import csv
 from geopy.geocoders import Nominatim
 
-if __name__ == '__main__':
-    cities = []
-    # geolocator = Nominatim(user_agent="Cities")
+from fitness import *
+from generate_solutions import *
+from genetic_representation import *
+from mutation import *
+from selection import *
+from crossover import *
 
-    with open('./european_cities.csv', 'r') as csvfile:
+CITIES_FILE = './european_cities.csv'
+NUMBER_OF_SOLUTIONS = 15
+NUMBER_OF_SELECTED_SOLUTIONS = 4
+
+
+def load_cities():
+    cities = []
+    with open(CITIES_FILE, 'r') as csvfile:
         # skip header
         csvfile.readline()
         for row in csv.reader(csvfile, delimiter=','):
             cities.append([row[0], float(row[1]), float(row[2])])
+    return cities
 
-print(cities)
+
+# prototype of first iteration
+cities = load_cities()
+solutions = generate_solutions(cities, NUMBER_OF_SOLUTIONS)
+solutions = sorted(solutions, key=lambda x: fitness_function(x, cities))
+selected_solutions = selection(solutions, NUMBER_OF_SELECTED_SOLUTIONS)
+crossovered_solutions = crossover(selected_solutions)
+mutated_solutions = mutation(crossovered_solutions)
+
+print('Best solution: {}'.format(solutions[0]))

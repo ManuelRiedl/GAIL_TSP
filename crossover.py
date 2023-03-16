ad7385d0a335a362f560f2a4e360881b1d78dadd
NicoOhler <32089075+NicoOhler@users.noreply.github.com>
2023-03-16 19:52:39 +0100

    Fix crossover return value

--- crossover.py
+++ crossover.py
@@ -5,28 +5,24 @@ In ordered crossover, we randomly select a subset of the first parent string.
 '''
 import random
 
-SPLIT_START = random.randint(0, 100)
-SPLIT_COUNT = random.randint(15, 25)
-
 
 def crossover_of_two(sol_one, sol_two):
+    split_start = random.randint(0, 100)
+    split_count = random.randint(15, 25)
+
     new_sol_two = []
     new_sol_one = []
-    new_sol_one.extend(sol_one[SPLIT_START:SPLIT_START + SPLIT_COUNT])
-    new_sol_two.extend(sol_two[SPLIT_START:SPLIT_START + SPLIT_COUNT])
+    new_sol_one.extend(sol_one[split_start:split_start + split_count])
+    new_sol_two.extend(sol_two[split_start:split_start + split_count])
 
-    counter = SPLIT_START
     for two_elm in sol_two:
-        for one_elm in sol_one[SPLIT_START:SPLIT_START + SPLIT_COUNT]:
+        for one_elm in sol_one[split_start:split_start + split_count]:
             if two_elm[0] == one_elm[0]:
                 new_sol_one.append(two_elm)
-                counter += 1
 
-    counter = SPLIT_START
     for one_elm in sol_one:
-        for two_elm in sol_two[SPLIT_START:SPLIT_START + SPLIT_COUNT]:
+        for two_elm in sol_two[split_start:split_start + split_count]:
             if one_elm[0] == two_elm[0]:
                 new_sol_two.append(one_elm)
-                counter += 1
 
-    return sol_one, sol_two
+    return [new_sol_one, new_sol_two]

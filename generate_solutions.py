212f5c31f19fc6e2e6d34a7aacd5dbbf9fb71202
Nico Ohler <nico.ohler@student.tugraz.at>
2023-03-16 16:29:49 +0100

    Implement solution generator

---
+++ generate_solutions.py
@@ -0,0 +1,15 @@
+'''
+    generate_solutions.py
+    
+    generates random solutions for the TSP problem
+'''
+import random
+
+
+def generate_solutions(cities, number_of_solutions):
+    solutions = []
+    for _ in range(number_of_solutions):
+        solution = list(range(len(cities)))
+        random.shuffle(solution)
+        solutions.append(solution)
+    return solutions
